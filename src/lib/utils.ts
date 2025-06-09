24ec2b91581fc47856f8e75b6f40f0c6e98d7de7
Oscar Herrera <a.oscarherrera@gmail.com>
2025-06-09 15:03:49 -0500

    chore: created functions to capilalize and lowercase breeds

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,21 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const formatBreeds = (breeds: string[] ) => {
+  const formatedBreeds = breeds.map((breed) => (
+    {
+      value: breed.toLowerCase(),
+      label: breed
+    }
+  ))
+  return formatedBreeds;
+}
+
+export const capitalizeBreed = (breed: string) => {
+  if(breed === "") return
+   const capitalizeBreed = breed.split(" ").map((word) => (
+          word[0].toUpperCase() + word.slice(1, word.length)
+        )).join(" ")
+    return capitalizeBreed
+}
