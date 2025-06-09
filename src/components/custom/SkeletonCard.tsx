9966edcf2c633277db05b2fd39d0e576c7518efa
Oscar Herrera <a.oscarherrera@gmail.com>
2025-06-09 14:55:44 -0500

    feat: created skeleton card for loading state

---
+++ src/components/custom/SkeletonCard.tsx
@@ -0,0 +1,29 @@
+import {
+  Card,
+  CardContent,
+  CardHeader,
+} from "@/components/ui/card";
+import { Skeleton } from "../ui/skeleton";
+
+const SkeletonCard = () => {
+  return (
+    <Card className="w-full max-w-xs py-0 pb-6">
+      <div className="aspect-video  rounded-t-lg overflow-hidden">
+        <Skeleton className="h-full w-full" />
+      </div>
+      <CardHeader>
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[250px]" />
+        </div>
+      </CardHeader>
+      <CardContent>
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[200px]" />
+          <Skeleton className="h-4 w-[175px]" />
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default SkeletonCard;
