50bba21d4d53d9ada1fd83fa5de8b1e84c25e15c
Oscar Herrera <a.oscarherrera@gmail.com>
2025-06-09 15:02:49 -0500

    chore: included types for dog & location resource

---
+++ src/types/locations.d.ts
@@ -0,0 +1,13 @@
+export interface ILocation {
+    zip_code: string
+    latitude: number
+    longitude: number
+    city: string
+    state: string
+    county: string
+}
+
+export interface ICoordinates {
+    lat: number;
+    lon: number;
+}
