611ccb2ff4931460636c27822313c198e68772ff
Oscar Herrera <a.oscarherrera@gmail.com>
2025-06-10 00:21:21 -0500

    fix: configured directory path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-import path from "path"
+import { fileURLToPath } from 'node:url'
+import { dirname, resolve } from 'node:path'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,7 +18,7 @@ export default defineConfig({
   ],
     resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": resolve(__dirname, "./src"),
     },
   },
 })
